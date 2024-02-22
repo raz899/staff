fbe6da9ae484964da301bca870cad576fefcc8fb
slava023 <vk_kiselev@pwrs.ru>
2024-02-22 17:01:41 +0300

    Редизайн staff - Редизайн таблицы в акциях

---
+++ scripts/documens.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.js-images-gallery img').forEach(img => {
+    img.onclick = () => {
+        document.querySelector('.pop-up').style.display = 'block';
+        document.querySelector('.pop-up img').src = img.getAttribute('src');
+    }
+});
+document.querySelector('.pop-up span').onclick = () => {
+    document.querySelector('.pop-up').style.display = 'none';
+}
