f3576f68a490cfb75af97147d65b63018d7fdc0b
slava023 <vk_kiselev@pwrs.ru>
2024-05-20 09:51:10 +0300

    Редизайн staff - Правки

--- scripts/index.js
+++ scripts/index.js
@@ -21,30 +21,20 @@ $menu.addEventListener('click', function (e) {
 });
 
 
+// добавление svg к link в классе .section-files
+function addSvgIcon(selector, iconId) {
+    document.querySelectorAll(selector).forEach(function (link) {
+        link.insertAdjacentHTML('afterbegin', '<svg width="18" height="18"><use href="/svg/common.bundle.svg#' + iconId + '"></use></svg>');
+    });
+}
 
-// pdf svg
-const pdfLinks = document.querySelectorAll('.section-files a[href$=".pdf"], .section-files a[href$=".PDF"], .section-files__link[href$=".pdf"]');
-// word svg
-const wordLinks = document.querySelectorAll('a[href$=".doc"],  a[href$=".docx"],  a[href$=".rtf"]');
-// other svg links
-const otherLinks = document.querySelectorAll('.section-files a:not([href$=".pdf"]):not([href$=".PDF"]):not([href$=".doc"]):not([href$=".docx"]):not([href$=".rtf"])');
+addSvgIcon('.section-files a[href$=".pdf"], .section-files a[href$=".PDF"], .section-files__link[href$=".pdf"]', 'pdf');
+addSvgIcon('a[href$=".doc"],  a[href$=".docx"],  a[href$=".rtf"]', 'word');
+addSvgIcon('.section-files a:not([href$=".pdf"]):not([href$=".PDF"]):not([href$=".doc"]):not([href$=".docx"]):not([href$=".rtf"])', 'other-links');
 
-pdfLinks.forEach(link => {
-    link.insertAdjacentHTML('afterbegin', `<svg width="16" height="16">
-        <use href = "/svg/common.bundle.svg#pdf"></use></svg >`);
-});
 
-wordLinks.forEach(link => {
-    link.insertAdjacentHTML('afterbegin', `<svg width="16" height="16">
-        <use href = "/svg/common.bundle.svg#word"></use></svg >`);
-});
 
-otherLinks.forEach(link => {
-    link.insertAdjacentHTML('afterbegin', `<svg width="16" height="16">
-        <use href="/svg/common.bundle.svg#other-links"></use></svg>`);
-});
 // popover
-
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
 var popoverList = popoverTriggerList.map(function (popoverTrigger) {
     return new bootstrap.Popover(popoverTrigger);
@@ -58,33 +48,7 @@ document.addEventListener('shown.bs.popover', function (event) {
         }
     });
 });
-const jsButtom = document.querySelectorAll('.js-button');
-if (jsButtom != null) {
-    document.querySelectorAll('.js-button').forEach(function (button) {
-        button.addEventListener('click', function () {
-            button.nextElementSibling.classList.toggle('d-none');
-            // document.querySelector('.js-generate-report').classList.toggle('d-none');
-        });
-    });
-}
 
-// При клике на ms-select-all отмечаем все checkbox
-const selectAllCheckbox = document.querySelector('.ms-select-all input');
-if (selectAllCheckbox != null) {
-    selectAllCheckbox.addEventListener('click', function () {
-        const checkboxes = document.querySelectorAll('.ms-select input');
-
-        if (selectAllCheckbox.checked) {
-            checkboxes.forEach(function (checkbox) {
-                checkbox.checked = true;
-            });
-        } else {
-            checkboxes.forEach(function (checkbox) {
-                checkbox.checked = false;
-            });
-        }
-    });
-}
 // увеличение картинки по клику
 const imgsgGallery = document.querySelectorAll('.js-images-gallery img');
 const popUp = document.querySelector('.pop-up span');
